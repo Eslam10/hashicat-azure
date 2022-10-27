b15fe0be724679dd5d8cf1c18e9283d8ce41a4e0
Eslam <eslamsayedattiama@gmail.comm>
2022-10-27 14:20:33 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/TF_Training_Eslam/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "eslam_rg"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
